84f7b7b8fd0a135963d755bf16fd3bd2d6f98b25
ghifar1327 <givartorreto@gmail.com>
2025-09-03 17:50:24 +0700

    Add files via upload

--- minitask2.js
+++ minitask2.js
@@ -1,8 +1,20 @@
-//buat segitiga siku-siku tidak mengunakan fungsi
-for (x = 1; x <= 5; x++) {
-  let sum = "";
-  for (y = 1; y <= x; y++) {
-    sum += "*";
+// buat segitiga siku-siku tidak mengunakan fungsi
+for (i = 1; i <= 5; i++) {
+  let s = "";
+  for (k = 1; k <= i; k++) {
+    s += "*";
   }
-  console.log(sum);
+  console.log(s);
 }
+
+let x = 1;
+let btg = "";
+do {
+  let y = x;
+  do {
+    btg += "*";
+    console.log(btg);
+    y++;
+  } while (y <= x);
+  x++;
+} while (x <= 5);
